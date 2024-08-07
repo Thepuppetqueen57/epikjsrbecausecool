dfbe1a7413b035469c8e1e67c1885acecafae946
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2024-08-06 19:48:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Coolness
+ This package is cool or something.
+
+ This package is actually just a way for me to make a goofy js package (tried with npm but npm dum)
---
+++ mod.ts
@@ -0,0 +1,14 @@
+export function epikjsr(bad: boolean) {
+    if(bad == true) {
+        console.error("MY BRLALS")
+    } else {
+        console.log("The most epik package of them all")
+    }
+}
+
+// Totally isnt taken right from epikcoolpackage on pub
+export function adder(epiknumber: number) : number {
+    const resultargh = epiknumber + 3
+
+    return resultargh
+}
